e7dffc883dd24c4ffeb32ecbf5a59897ee69bd4a
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-12-07 20:55:16 +0000

    make it ready for seperate data
    alien biome trees are wrong and seem to need to use different selectors

---
+++ utility/functions/biome-trees-data/alien-biomes.lua
@@ -0,0 +1,306 @@
+-- Alien Biomes - 0.6.1
+local Data = {}
+
+Data.GetTileData = function()
+    return {
+        ["frozen-snow-0"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-1"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-2"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-3"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-4"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-5"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-6"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-7"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-8"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["frozen-snow-9"] = {
+            "allow-trees",
+            {
+                {
+                    -0.5
+                },
+                {
+                    0
+                }
+            }
+        },
+        ["mineral-aubergine-dirt-1"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0.4
+                },
+                {
+                    0.6,
+                    0.6
+                }
+            }
+        },
+        ["mineral-aubergine-dirt-2"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0.4
+                },
+                {
+                    0.6,
+                    0.6
+                }
+            }
+        },
+        ["mineral-aubergine-dirt-3"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0.6
+                },
+                {
+                    0.6,
+                    0.8
+                }
+            }
+        },
+        ["mineral-aubergine-dirt-4"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0.6
+                },
+                {
+                    0.6,
+                    0.95
+                }
+            }
+        },
+        ["mineral-aubergine-dirt-5"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0.4
+                },
+                {
+                    0.6,
+                    0.6
+                }
+            }
+        },
+        ["mineral-aubergine-dirt-6"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0.4
+                },
+                {
+                    0.6,
+                    0.6
+                }
+            }
+        },
+        ["mineral-cream-sand-1"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0
+                },
+                {
+                    0.6,
+                    0.4
+                }
+            }
+        },
+        ["mineral-cream-sand-2"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0
+                },
+                {
+                    0.6,
+                    0.4
+                }
+            }
+        },
+        ["mineral-cream-sand-3"] = {
+            "allow-trees",
+            {
+                {
+                    0.3,
+                    0
+                },
+                {
+                    0.6,
+                    0.4
+                }
+            }
+        },
+        ["vegetation-violet-grass-1"] = {
+            "allow-trees",
+            {
+                {
+                    0.6,
+                    0.6
+                },
+                {
+                    0.8,
+                    1
+                }
+            }
+        },
+        ["vegetation-violet-grass-2"] = {
+            "allow-trees",
+            {
+                {
+                    0.6,
+                    0.6
+                },
+                {
+                    0.8,
+                    1
+                }
+            }
+        },
+        ["volcanic-green-heat-1"] = {
+            "allow-trees",
+            {
+                {
+                    1
+                },
+                {
+                    1.2
+                }
+            }
+        },
+        ["volcanic-green-heat-2"] = {
+            "allow-trees",
+            {
+                {
+                    1.2
+                },
+                {
+                    1.3500000000000001
+                }
+            }
+        },
+        ["volcanic-green-heat-3"] = {
+            "allow-trees",
+            {
+                {
+                    1.3500000000000001
+                },
+                {
+                    1.45
+                }
+            }
+        },
+        ["volcanic-green-heat-4"] = {
+            "allow-trees",
+            {
+                {
+                    1.45
+                },
+                {
+                    1.5
+                }
+            }
+        }
+    }
+end
+
+return Data
--- utility/functions/biome-trees.lua
+++ utility/functions/biome-trees.lua
@@ -6,8 +6,11 @@
 local Utils = require("utility/utils")
 local Logging = require("utility/logging")
 
+local BaseGameData = require("utility/functions/biome-trees-data/base-game")
+local AlienBiomesData = require("utility/functions/biome-trees-data/alien-biomes")
+
 local BiomeTrees = {}
-local logNonPositives = false
+local logNonPositives = true
 local logPositives = false
 local logData = false
 
@@ -26,7 +29,7 @@ BiomeTrees.GetBiomeTreeName = function(surface, position)
             return BiomeTrees._GetTruelyRandomTreeForTileCollision(tile)
         end
     end
-    if tileData.type == "water" or tileData.type == "no-trees" then
+    if tileData.type ~= "allow-trees" then
         return nil
     end
 
@@ -195,36 +198,14 @@ BiomeTrees._GetTileData = function()
         end
         tileDetails[tileName] = {name = tileName, type = type, tempRanges = tempRanges, moistureRanges = moistureRanges}
     end
+    local function AddTilesDetails(tilesDetails)
+        for name, details in pairs(tilesDetails) do
+            AddTileDetails(name, details[1], details[2], details[3])
+        end
+    end
 
-    -- Vanilla - 1.1.5
-    AddTileDetails("grass-1", "grass", {{0, 0.7}, {1, 1}})
-    AddTileDetails("grass-2", "grass", {{0.45, 0.45}, {1, 0.8}})
-    AddTileDetails("grass-3", "grass", {{0, 0.6}, {0.65, 0.9}})
-    AddTileDetails("grass-4", "grass", {{0, 0.5}, {0.55, 0.7}})
-    AddTileDetails("dry-dirt", "dirt", {{0.45, 0}, {0.55, 0.35}})
-    AddTileDetails("dirt-1", "dirt", {{0, 0.25}, {0.45, 0.3}}, {{0.4, 0}, {0.45, 0.25}})
-    AddTileDetails("dirt-2", "dirt", {{0, 0.3}, {0.45, 0.35}})
-    AddTileDetails("dirt-3", "dirt", {{0, 0.35}, {0.55, 0.4}})
-    AddTileDetails("dirt-4", "dirt", {{0.55, 0}, {0.6, 0.35}}, {{0.6, 0.3}, {1, 0.35}})
-    AddTileDetails("dirt-5", "dirt", {{0, 0.4}, {0.55, 0.45}})
-    AddTileDetails("dirt-6", "dirt", {{0, 0.45}, {0.55, 0.5}})
-    AddTileDetails("dirt-7", "dirt", {{0, 0.5}, {0.55, 0.55}})
-    AddTileDetails("sand-1", "sand", {{0, 0}, {0.25, 0.15}})
-    AddTileDetails("sand-2", "sand", {{0, 0.15}, {0.3, 0.2}}, {{0.25, 0}, {0.3, 0.15}})
-    AddTileDetails("sand-3", "sand", {{0, 0.2}, {0.4, 0.25}}, {{0.3, 0}, {0.4, 0.2}})
-    AddTileDetails("red-desert-0", "desert", {{0.55, 0.35}, {1, 0.5}})
-    AddTileDetails("red-desert-1", "desert", {{0.6, 0}, {0.7, 0.3}}, {{0.7, 0.25}, {1, 0.3}})
-    AddTileDetails("red-desert-2", "desert", {{0.7, 0}, {0.8, 0.25}}, {{0.8, 0.2}, {1, 0.25}})
-    AddTileDetails("red-desert-3", "desert", {{0.8, 0}, {1, 0.2}})
-    AddTileDetails("water", "water")
-    AddTileDetails("deepwater", "water")
-    AddTileDetails("water-green", "water")
-    AddTileDetails("deepwater-green", "water")
-    AddTileDetails("water-shallow", "water")
-    AddTileDetails("water-mud", "water")
-    AddTileDetails("out-of-map", "no-trees")
-    AddTileDetails("landfill", "desert", {{0, 0}, {0.25, 0.15}}) --same as sand-1
-    AddTileDetails("nuclear-ground", "desert", {{0, 0}, {0.25, 0.15}}) --same as sand-1
+    AddTilesDetails(BaseGameData.GetTileData())
+    AddTilesDetails(AlienBiomesData.GetTileData())
 
     return tileDetails
 end
