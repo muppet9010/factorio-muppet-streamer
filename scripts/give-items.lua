ba0ac3dd6172bdbe36345c336b92ebf7357ea3e4
Muppet9010 <mike.hollett@gmail.com>
2022-05-07 00:14:37 +0100

    Leaky flamethrower removed weapon returned
    Testing of this and previous GiveItems changes.
    EmmyLua additions and some code tidyup to aid documenting.

--- scripts/give-items.lua
+++ scripts/give-items.lua
@@ -5,6 +5,22 @@ local Logging = require("utility/logging")
 local EventScheduler = require("utility/event-scheduler")
 local Utils = require("utility/utils")
 
+---@class GiveItems_GiveWeaponAmmoScheduled
+---@field target string @ Target player's name.
+---@field ammoType LuaItemPrototype
+---@field ammoCount uint
+---@field weaponType LuaItemPrototype
+---@field forceWeaponToSlot boolean
+---@field selectWeapon boolean
+
+---@class GiveItems_RemovedWeaponToEnsureWeapon
+---@field gunInventoryIndex uint @ The index in the gun and ammo inventory where the weapon was removed.
+---@field weaponItemName string|null @ Nil if no weapon was in the slot.
+---@field weaponFilterName string|null @ Nil if no weapon filter was set on the slot.
+---@field ammoItemName string|null @ Nil if no ammo was in the slot.
+---@field ammoFilterName string|null @ Nil if no ammo filter was set on the slot.
+---@field beforeSelectedWeaponGunIndex uint @ The weapon slot that the player had selected before the weapon was removed.
+
 GiveItems.CreateGlobals = function()
     global.giveItems = global.giveItems or {}
     global.giveItems.nextId = global.giveItems.nextId or 0
@@ -22,14 +38,19 @@ end
 ---@param forceWeaponToWeaponInventorySlot boolean
 ---@param selectWeapon boolean
 ---@return boolean|null weaponGiven @ If the weapon item had to be given to the player, compared to them already having it and it possibly just being mvoed between their inventories. Returns nil for invalid situations, i.e. called on a palyer with no gun inventory.
+---@return GiveItems_RemovedWeaponToEnsureWeapon|null removedWeaponDetails @ Details on the weapon that was removed to add the new wepaon. Is nil if no active weapon was set/found, i.e. weapon was found/put in to the players main inventory and not as an equiped weapon.
 GiveItems.EnsureHasWeapon = function(player, weaponName, forceWeaponToWeaponInventorySlot, selectWeapon)
     if player == nil or not player.valid then
         return nil
     end
 
+    ---@type GiveItems_RemovedWeaponToEnsureWeapon
+    local removedWeaponDetails = {
+        beforeSelectedWeaponGunIndex = player.character.selected_gun_index
+    }
+
     -- See if the gun is already equipped by the player in their active gun inventory, or find which of their weapon slots is best to assign too.
-    local weaponGiven = false
-    local removedWeaponFilter, removedAmmoFilter, weaponFoundIndex
+    local weaponGiven, weaponFoundIndex, freeGunIndex, freeButFilteredGunIndex = false, nil, nil, nil
     local gunInventory = player.get_inventory(defines.inventory.character_guns)
     if gunInventory == nil then
         return nil
@@ -47,8 +68,9 @@ GiveItems.EnsureHasWeapon = function(player, weaponName, forceWeaponToWeaponInve
             -- No weapon in slot.
             local filteredName = gunInventory.get_filter(i)
             if filteredName == nil or filteredName == weaponName then
-                -- Non filtered weapon slot or fitlered to the weapon we want to assign.
+                -- Non filtered weapon slot or filtered to the weapon we want to assign.
                 freeGunIndex = i
+                break
             else
                 -- Filtered weapon slot to a different weapon.
                 freeButFilteredGunIndex = i
@@ -80,25 +102,16 @@ GiveItems.EnsureHasWeapon = function(player, weaponName, forceWeaponToWeaponInve
                 -- Clear the gun slot ready for the weapon.
                 local gunItemStack = gunInventory[weaponFoundIndex]
                 if gunItemStack ~= nil and gunItemStack.valid_for_read then
-                    local gunInsertedCount = player.insert({name = gunItemStack.name, count = gunItemStack.count})
-                    if gunInsertedCount < gunItemStack.count then
-                        player.surface.spill_item_stack(player.position, {name = gunItemStack.name, count = gunItemStack.count - gunInsertedCount}, true, nil, false)
+                    local currentName, currentCount = gunItemStack.name, gunItemStack.count
+                    local gunInsertedCount = player.insert({name = currentName, count = currentCount})
+                    if gunInsertedCount < currentCount then
+                        player.surface.spill_item_stack(player.position, {name = currentName, count = currentCount - gunInsertedCount}, true, nil, false)
                     end
+                    removedWeaponDetails.weaponItemName = currentName
                 end
+                removedWeaponDetails.weaponFilterName = gunInventory.get_filter(weaponFoundIndex)
                 gunInventory.set_filter(weaponFoundIndex, nil)
                 gunItemStack.clear()
-
-                -- Clear the ammo slot ready for the weapon and its possible ammo.
-                local ammoInventory = player.get_inventory(defines.inventory.character_ammo)
-                local ammoItemStack = ammoInventory[weaponFoundIndex]
-                if ammoItemStack ~= nil and ammoItemStack.valid_for_read then
-                    local ammoInsertedCount = player.insert({name = ammoItemStack.name, count = ammoItemStack.count,ammo = ammoItemStack.ammo})
-                    if ammoInsertedCount < ammoItemStack.count then
-                        player.surface.spill_item_stack(player.position, {name = ammoItemStack.name, count = ammoItemStack.count - ammoInsertedCount}, true, nil, false)
-                    end
-                end
-                ammoInventory.set_filter(weaponFoundIndex, nil)
-                ammoItemStack.clear()
             else
                 -- As we won't force the weapon it should go in to the characters inventory if they don't already have one.
                 -- As we can't select the weapon the function is done after this.
@@ -113,20 +126,32 @@ GiveItems.EnsureHasWeapon = function(player, weaponName, forceWeaponToWeaponInve
             end
         end
 
-        -- If a weapon index for the gun has been found then move/add one there.
-        if weaponFoundIndex ~= nil then
-            -- Remove 1 item of the weapon type from the players inventory if they had one, to simulate equiping the weapon. Otherwise we will flag this as giving the player a weapon.
-            if characterInventory.get_item_count(weaponName) == 0 then
-                -- No instacne of the weapon in the player's inventory.
-                weaponGiven = true
-            else
-                -- Weapon in players inventory, so remove 1.
-                characterInventory.remove({name = weaponName, count = 1})
+        -- Clear the ammo slot ready for the weapon and its possible ammo.
+        local ammoInventory = player.get_inventory(defines.inventory.character_ammo)
+        local ammoItemStack = ammoInventory[weaponFoundIndex]
+        if ammoItemStack ~= nil and ammoItemStack.valid_for_read then
+            local currentName, currentCount = ammoItemStack.name, ammoItemStack.count
+            local ammoInsertedCount = player.insert({name = currentName, count = currentCount, ammo = ammoItemStack.ammo})
+            if ammoInsertedCount < currentCount then
+                player.surface.spill_item_stack(player.position, {name = currentName, count = currentCount - ammoInsertedCount}, true, nil, false)
             end
-
-            -- Put the weapon in the player's actual gun slot.
-            gunInventory[weaponFoundIndex].set_stack({name = weaponName, count = 1})
+            removedWeaponDetails.ammoItemName = currentName
         end
+        removedWeaponDetails.ammoFilterName = ammoInventory.get_filter(weaponFoundIndex)
+        ammoInventory.set_filter(weaponFoundIndex, nil)
+        ammoItemStack.clear()
+
+        -- Remove 1 item of the weapon type from the players inventory if they had one, to simulate equiping the weapon. Otherwise we will flag this as giving the player a weapon.
+        if characterInventory.get_item_count(weaponName) == 0 then
+            -- No instacne of the weapon in the player's inventory.
+            weaponGiven = true
+        else
+            -- Weapon in players inventory, so remove 1.
+            characterInventory.remove({name = weaponName, count = 1})
+        end
+
+        -- Put the weapon in the player's actual gun slot.
+        gunInventory[weaponFoundIndex].set_stack({name = weaponName, count = 1})
     end
 
     -- Set the players active weapon if this is desired.
@@ -134,9 +159,13 @@ GiveItems.EnsureHasWeapon = function(player, weaponName, forceWeaponToWeaponInve
         player.character.selected_gun_index = weaponFoundIndex
     end
 
-    return weaponGiven
+    -- Record the weapon index that was used in the end.
+    removedWeaponDetails.gunInventoryIndex = weaponFoundIndex
+
+    return weaponGiven, removedWeaponDetails
 end
 
+---@param command CustomCommandData
 GiveItems.GivePlayerWeaponAmmoCommand = function(command)
     local errorMessageStart = "ERROR: muppet_streamer_give_player_weapon_ammo command "
     local commandData
@@ -213,7 +242,7 @@ GiveItems.GivePlayerWeaponAmmoCommand = function(command)
 end
 
 GiveItems.GiveWeaponAmmoScheduled = function(eventData)
-    local data = eventData.data
+    local data = eventData.data ---@type GiveItems_GiveWeaponAmmoScheduled
 
     local targetPlayer = game.get_player(data.target)
     if targetPlayer == nil or not targetPlayer.valid then
